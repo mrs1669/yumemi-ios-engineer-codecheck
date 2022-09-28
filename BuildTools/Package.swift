94870059f4cf35ac08f536df919d04e4a7c07f99
mrs1669 <mrs1669.develop@gmail.com>
2022-09-28 20:39:57 +0900

    [chore] add Package.swift

---
+++ BuildTools/Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "AppFeature",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(name: "AppFeature", targets: ["AppFeature"]),
+    ],
+    dependencies: [
+        // Local Packages
+        .package(path: "../Core"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"
+        ),
+        .plugin(
+            name: "SwiftLintXcode",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
+        .target(
+            name: "AppFeature",
+            dependencies: [
+                "Core"
+            ],
+            plugins: ["SwiftLintXcode"]),
+        .testTarget(
+            name: "AppFeatureTests",
+            dependencies: ["AppFeature"]),
+    ]
+)
