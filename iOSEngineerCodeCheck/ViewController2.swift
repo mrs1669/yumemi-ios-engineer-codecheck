63d93910d4fddbdcb4d697e8fa484733df32d614
mrs1669 <mrs1669.develop@gmail.com>
2022-09-29 18:13:12 +0900

    [fix] delete line

--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -11,22 +11,16 @@ import UIKit
 class ViewController2: UIViewController {
 
     @IBOutlet private weak var imgView: UIImageView!
-
     @IBOutlet private weak var ttlLbl: UILabel!
-
     @IBOutlet private weak var langLbl: UILabel!
-
     @IBOutlet private weak var strsLbl: UILabel!
     @IBOutlet private weak var wchsLbl: UILabel!
     @IBOutlet private weak var frksLbl: UILabel!
     @IBOutlet private weak var isssLbl: UILabel!
-
     var vc1: ViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //let repo = vc1.repo[vc1.idx]
         let items = vc1.items[vc1.idx]
         guard let language = items.language else { return }
         langLbl.text = "Written in \(language)"
@@ -35,19 +29,15 @@ class ViewController2: UIViewController {
         frksLbl.text = "\(items.forksCount) forks"
         isssLbl.text = "\(items.openIssuesCount) open issues"
         getImage()
-
     }
 
     func getImage() {
-
         let repo = vc1.items[vc1.idx]
-
         ttlLbl.text = repo.fullName
         guard let avatarUrl = URL(string: repo.owner.avatarUrl) else { return }
         URLSession.shared.dataTask(with: avatarUrl) { [weak self] (data, _, _) in
             guard let data = data else { return }
             guard let avatarImage = UIImage(data: data) else { return }
-
             DispatchQueue.main.async {
                 self?.imgView.image = avatarImage
             }
