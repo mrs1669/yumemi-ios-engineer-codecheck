307112191021e7c098b03aa43e3e25b7d066ef80
mrs1669 <mrs1669.develop@gmail.com>
2022-09-29 13:15:26 +0900

    [feat] set struct SearchRepository

--- iOSEngineerCodeCheck/Entity/SearchRepository.swift
+++ iOSEngineerCodeCheck/Entity/SearchRepository.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+struct SearchRepository: Codable {
+    let items: [Item]
+}
+
+struct Item: Codable {
+    let id: Int
+    let fullName: String
+    let language: String?
+    let stargazersCount: Int
+    let watchersCount: Int
+    let forksCount: Int
+    let openIssuesCount: Int
+    let owner: Owner
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case fullName = "full_name"
+        case language = "language"
+        case stargazersCount = "stargazers_count"
+        case watchersCount = "watchers_count"
+        case forksCount = "forks_count"
+        case openIssuesCount = "open_issues_count"
+        case owner = "owner"
+    }
+}
+
+struct Owner: Codable {
+    let avatarUrl: String
+
+    private enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+    }
+}
