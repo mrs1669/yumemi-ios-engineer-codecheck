b239f18f25a97a09ce8bf1f980e9e9fb7725dd82
mrs1669 <mrs1669.develop@gmail.com>
2022-09-30 12:59:46 +0900

    [feat] add presenter SearchRepositoryPresenter

--- iOSEngineerCodeCheck/SearchRepository/SearchRepositoryPresenter.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryPresenter.swift
@@ -8,6 +8,44 @@
 
 import Foundation
 
+protocol SearchRepositoryPresenterInputProtocol {
+    var repositories: [Item] { get }
+    func repository(forRow row: Int) -> Item?
+    func viewDidLoad(keyword: String)
+}
+
 protocol SearchRepositoryPresenterOutputProtocol: AnyObject {
     func updateRepositories(_ repositories: [Item])
+}
+
+final class SearchRepositoryPresenter: SearchRepositoryPresenterInputProtocol {
+    private(set) var repositories: [Item] = []
+    private weak var view: SearchRepositoryPresenterOutputProtocol!
+    private var model: SearchRepositoryModelProtocol
+
+    init(view: SearchRepositoryPresenterOutputProtocol, model: SearchRepositoryModelProtocol) {
+        self.view = view
+        self.model = model
+    }
+
+    func repository(forRow row: Int) -> Item? {
+        guard row < repositories.count else { return nil }
+        return repositories[row]
+    }
+
+    func viewDidLoad(keyword: String) {
+        if !keyword.isEmpty {
+            model.fetchRepositories(keyword: keyword) { [weak self] result in
+                switch result {
+                case .success(let repositories):
+                    self?.repositories = repositories
+                    DispatchQueue.main.async {
+                        self?.view.updateRepositories(repositories)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
 }
