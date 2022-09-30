f8b714ae7c44f945032903601f2a309fdb301a76
mrs1669 <mrs1669.develop@gmail.com>
2022-09-30 12:15:28 +0900

    [feat] make SearchRepositoryPresenter.swift

---
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SearchRepositoryPresenter.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 村石拓海 on 2022/09/30.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
