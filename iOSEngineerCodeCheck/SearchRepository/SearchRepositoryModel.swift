7585f2e11c8287b0af1fb61d4dd0f27bf515c9d9
mrs1669 <mrs1669.develop@gmail.com>
2022-09-30 12:13:42 +0900

    [chore] add comment

--- iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
@@ -16,7 +16,8 @@ protocol SearchRepositoryModelProtocol {
 final class SearchRepositoryModel: SearchRepositoryModelProtocol {
     private var requestUrlString: String!
     private var task: URLSessionTask?
-
+    
+    // MARK: Fetch Repository
     func fetchRepositories(keyword: String, completion: @escaping (Result<[Item], Error>) -> Void) {
         guard let encodeKeywordString = keyword.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else { return }
         requestUrlString = "https://api.github.com/search/repositories?q=\(encodeKeywordString)"
