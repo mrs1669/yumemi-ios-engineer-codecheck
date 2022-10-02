e259da5cc9598463d8fab52324662b7621777dc9
mrs1669 <mrs1669.develop@gmail.com>
2022-10-02 18:31:36 +0900

    [feat] create SearchRepositoryTableViewCell.swift

---
+++ iOSEngineerCodeCheck/SearchRepository/Cell/SearchRepositoryTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SearchRepositoryTableViewCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 村石拓海 on 2022/10/02.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
