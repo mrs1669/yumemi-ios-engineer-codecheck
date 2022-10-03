40ea4969b72275dda3ea48ba26f6ff1b83078706
mrs1669 <mrs1669.develop@gmail.com>
2022-10-03 19:22:18 +0900

    [feat] add background assets color

--- generated/R.generated.swift
+++ generated/R.generated.swift
@@ -147,10 +147,21 @@ struct R: Rswift.Validatable {
   }
   #endif
 
-  /// This `R.color` struct is generated, and contains static references to 1 colors.
+  /// This `R.color` struct is generated, and contains static references to 2 colors.
   struct color {
+    /// Color `Background`.
+    static let background = Rswift.ColorResource(bundle: R.hostingBundle, name: "Background")
     /// Color `MainTheme`.
     static let mainTheme = Rswift.ColorResource(bundle: R.hostingBundle, name: "MainTheme")
+
+    #if os(iOS) || os(tvOS)
+    /// `UIColor(named: "Background", bundle: ..., traitCollection: ...)`
+    @available(tvOS 11.0, *)
+    @available(iOS 11.0, *)
+    static func background(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIColor? {
+      return UIKit.UIColor(resource: R.color.background, compatibleWith: traitCollection)
+    }
+    #endif
 
     #if os(iOS) || os(tvOS)
     /// `UIColor(named: "MainTheme", bundle: ..., traitCollection: ...)`
@@ -158,6 +169,14 @@ struct R: Rswift.Validatable {
     @available(iOS 11.0, *)
     static func mainTheme(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIColor? {
       return UIKit.UIColor(resource: R.color.mainTheme, compatibleWith: traitCollection)
+    }
+    #endif
+
+    #if os(watchOS)
+    /// `UIColor(named: "Background", bundle: ..., traitCollection: ...)`
+    @available(watchOSApplicationExtension 4.0, *)
+    static func background(_: Void = ()) -> UIKit.UIColor? {
+      return UIKit.UIColor(named: R.color.background.name)
     }
     #endif
 
